6ffc7eac027b3e5e47058bba21f3ee17992f7ed6
ZacharyTF <70725593+ZacharyTF@users.noreply.github.com>
2021-02-05 22:49:53 -0500

    Add files via upload

---
+++ DLExam/DLExam/Drivers_License_Exam/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Drivers_License_Exam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnGrade = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(215, 9);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(254, 116);
+            this.listBox1.TabIndex = 0;
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDisplay.Location = new System.Drawing.Point(13, 9);
+            this.lblDisplay.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(194, 53);
+            this.lblDisplay.TabIndex = 1;
+            // 
+            // btnGrade
+            // 
+            this.btnGrade.Location = new System.Drawing.Point(13, 66);
+            this.btnGrade.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGrade.Name = "btnGrade";
+            this.btnGrade.Size = new System.Drawing.Size(194, 29);
+            this.btnGrade.TabIndex = 2;
+            this.btnGrade.Text = "Choose File and Grade";
+            this.btnGrade.UseVisualStyleBackColor = true;
+            this.btnGrade.Click += new System.EventHandler(this.btnGrade_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 99);
+            this.button1.Name = "exitButton";
+            this.button1.Size = new System.Drawing.Size(190, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 141);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGrade);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnGrade;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ DLExam/DLExam/Drivers_License_Exam/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/**
+ * 02-05-2021
+ * CSC 153
+ * Zachary Fegan
+ * Program that takes answers for a 20 question test and compares them to the actual answers to determine if the student passed. Then Displays the amount of questions answered correctly, incorrectly, and a list of numbers
+ * for the questions that were answered incorrectly
+ */
+
+namespace Drivers_License_Exam
+{
+    public partial class Form1 : Form
+    {
+        readonly char[] CORRECT_ANSWERS = new char[20] { 'B', 'D', 'A', 'A', 'C',
+                                                         'A', 'B', 'A', 'C', 'D',
+                                                         'B', 'C', 'D', 'A', 'D',
+                                                         'C', 'C', 'B', 'D', 'A' };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGrade_Click(object sender, EventArgs e)
+        {
+            lblDisplay.Text = "";
+            listBox1.Items.Clear();
+
+            char[] studentAnswers = GetStudentAnswers();
+
+            GradeTest(studentAnswers);
+        }
+
+        public char[] GetStudentAnswers()
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    char[] studentAnswers = new char[20];
+                    StreamReader inputFile = File.OpenText(openFileDialog1.FileName);
+
+
+                    int index = 0;
+                    while (index < studentAnswers.Length && !inputFile.EndOfStream)
+                    {
+                        studentAnswers[index] = char.Parse(inputFile.ReadLine());
+                        index++;
+                    }
+
+                    inputFile.Close();
+                    return studentAnswers;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return null;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Operation cancelled.");
+                return null;
+            }
+        }
+
+        public void GradeTest(char[] studentAnswers)
+        {
+            int score = 0;
+            string Result;
+
+            for (int index = 0; index < CORRECT_ANSWERS.Length; index++)
+            {
+                if (CORRECT_ANSWERS[index] == studentAnswers[index])
+                {
+                    score++;
+                }
+                else
+                {
+                    listBox1.Items.Add($"{index + 1}. \t{studentAnswers[index]} \t(Correct = {CORRECT_ANSWERS[index]})");
+                }
+            }
+
+            if (score >= 15)
+            {
+                Result = "You passed the exam.";
+            }
+            else
+            {
+                Result = "You failed the exam.";
+            }
+
+            lblDisplay.Text = $"{score} correctly answered \n{20 - score} incorrectly answered \n"+ Result;
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
